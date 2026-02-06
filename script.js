c107e8783f996c61cf9db35233e08836a32f4c25
Tema Yurchenko <temaunique3@gmail.com>
2026-02-06 12:48:28 +0300

    ADD css and images

---
+++ script.js
@@ -0,0 +1,35 @@
+const tabsBtns = document.querySelectorAll(".tabs__nav button");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+function hideTabs() {
+    tabsItems.forEach(tab => tab.classList.add('hide'));
+    tabsBtns.forEach(btn => btn.classList.remove('active'));
+}
+
+function showTab(index) {
+    tabsItems[index].classList.remove('hide');
+    tabsBtns[index].classList.add('active');
+}
+
+hideTabs();
+showTab(0);
+
+tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
+    hideTabs();
+    showTab(index);
+}))
+
+// Якори
+const anchors = document.querySelectorAll(".header__nav a");
+
+anchors.forEach(anc => {
+    anc.addEventListener("click", function(event) {
+        event.preventDefault();
+        const id = anc.getAttribute("href");
+        const elem = document.querySelector(id);
+        window.scroll({
+            top: elem.offsetTop - 60, 
+            behavior: "smooth"
+        })
+    });
+});
